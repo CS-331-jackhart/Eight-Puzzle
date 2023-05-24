2f6cb3a47dc072fcf32efbffdf125093210e0c61
Endersky78 <jackhart591@gmail.com>
2023-05-23 17:57:23 -0700

    Forgot to commit these changes

--- agent.py
+++ agent.py
@@ -1,35 +1,49 @@
 from __future__ import annotations
 from board import Board
 from collections.abc import Callable
+from queue import PriorityQueue
 import copy
 
 class BoardState:
     def __init__(self, board: Board, move: str, pastMoves):
         self.board = board
+        self.pastMoves = copy.deepcopy(pastMoves)
 
         if move != '':
-            pastMoves.append(move)
+            self.pastMoves.append(move)
 
-        self.pastMoves = pastMoves
+    def Seen(self, seenDict):
 
-    def Seen(self, seenList):
-        for string_outer_seen in seenList:
-            for (string_inner_seen, new_str) in zip(string_outer_seen, self.pastMoves):
-                if string_inner_seen != new_str:
-                    print("has been!")
-                    return True
-        return False
+        if len(seenDict) == 0:
+            return False
+
+        if str(self.board) in seenDict:
+            if len(seenDict[str(self.board)]) <= len(self.pastMoves):
+                return True
+            else:
+                return False
 
 '''
 Heuristics
 '''
 def MT(board: Board) -> int:
-    return 
+    solStr = "[[1 2 3]\n [4 5 6]\n [7 8 0]]"
+    return sum(1 for a, b in zip(str(board), solStr) if a != b)
 
 def CB(board: Board) -> int:
-    return 
+    sol = {0: (2, 2), 1: (0, 0), 2: (1, 0), 3: (2, 0), 4: (0, 1), 5: (1, 1), 6: (2, 1), 7: (0, 2), 8: (1, 2)}
+    total = 0
+
+    for y, row in enumerate(board.state):
+        for x, num in enumerate(row):
+            total += (abs(sol[num][0] - x) + abs(sol[num][1] - y))
+
+    return total
 
 def NA(board: Board) -> int:
+    return 9 - MT(board) # Returns the number of correct squares
+
+def BFS(board: Board) -> int:
     return 0
 
 
@@ -38,21 +52,23 @@ def NA(board: Board) -> int:
 A* Search 
 '''
 def a_star_search(board: Board, heuristic: Callable[[Board], int]):
-    frontier = []
-    seen = []
-    newState = BoardState(board, "", []) # Initial state
+    frontier = PriorityQueue()
+    seen = {}
+    currState = BoardState(board, "", [])
+    numNodesSearched = 0
 
     # While the board is not in a goal state
-    while not newState.board.goal_test():
-        for possibleMove in newState.board.next_action_states(): # Iterate over all possible moves
-            newState = BoardState(possibleMove[0], possibleMove[1], newState.pastMoves) # Create a new board state for this move
+    while not currState.board.goal_test():
+        for possibleMove in currState.board.next_action_states(): # Iterate over all possible moves
+            newState = BoardState(possibleMove[0], possibleMove[1], currState.pastMoves) # Create a new board state for this move
 
             if not newState.Seen(seen): # Check if this string has been seen before
-                frontier.append(newState)
-                seen.append(newState.pastMoves)
+                priority = len(newState.pastMoves) + heuristic(newState.board)
+                frontier.put((priority, id(newState), newState)) # ID is there to be a tie breaker in case of identical priorities
+                seen[str(newState.board)] = newState.pastMoves
 
-        # Sort by the amount of moves + the heuristic func
-        frontier.sort(key=(lambda a : len(a.pastMoves) + heuristic(a)))
-        newState = frontier.pop(0)
-        
-    return newState.pastMoves
+        currState = frontier.get()[2]
+        numNodesSearched += 1
+
+    return (numNodesSearched, currState.pastMoves)
+    # return currState.pastMoves <- This is for running with test.py
--- main.py
+++ main.py
@@ -1,22 +1,35 @@
+from agent import MT, CB, NA, BFS, a_star_search
 from board import Board
 import numpy as np
 import time
 
 def main():
 
-    for m in [10,20,30,40,50]:
-        for seed in range(0,10):
-            # Sets the seed of the problem so all students solve the same problems
-            board = Board(m, seed)
-            
-            start =  time.process_time()   
-            '''
-            ***********************************************
-            Solve the Board state here with A*
-            ***********************************************
-            '''
-            end =  time.process_time()
-            solution_cpu_time = end-start
+    string_dict = {MT: "Misplaced Tiles heuristic", CB: "City-Block Distance heuristic", NA: "Non-Admissible heuristic", BFS: "Breadth-First Search"}
+
+    for test in [MT, CB, NA, BFS]:
+        print("Testing %s..." % string_dict[test])
+        for m in [10,20,30,40,50]:
+            sum_cput = 0
+            sum_solLen = 0
+            sum_nodeS = 0
+            for seed in range(0,10): # Sets the seed of the problem so all students solve the same problems
+                board = Board(m, seed)
+                
+                start =  time.process_time()
+                data = a_star_search(board, test)
+                end =  time.process_time()
+                solution_cpu_time = end-start
+
+                sum_cput += solution_cpu_time
+                sum_solLen += len(data[1])
+                sum_nodeS += data[0]
+            print("= M=%d" % m)
+            print("== Average CPU Time: %0.3f" % (sum_cput / 10))
+            print("== Average Solution Length: %0.3f" % (sum_solLen / 10))
+            print("== Total nodes searched: %d" % sum_nodeS)
+            print()
+        print()
 
 if __name__ == "__main__":
     main()
